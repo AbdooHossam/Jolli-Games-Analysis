06efc8c1e716615123fdea30cd75baffad505272
Abdelrahman Hossam Ahmed <104042830+AbdooHossam@users.noreply.github.com>
2023-02-28 22:29:43 +0300

    Code Description
    This SQL code generates a report that breaks down the income earned from in-app purchases (IAPs) by age group and gender. It does this by first defining age groups based on the age of the user. The code then joins data from the event, Account, and Product tables to identify the price of each IAP and the age and gender of the user who made the purchase. Finally, the data is grouped by age group and gender and the total income for each group is summed and ordered by age group and gender.

--- Q3 Income for age & Gender.sql
+++ Q3 Income for age & Gender.sql
@@ -12,5 +12,5 @@ FROM event E
 JOIN Account A ON E.device_id = A.device_id
 JOIN Product P ON E.event_name = P.event_name
 WHERE E.event_name IN ('App_Purchase_001', 'App_Purchase_002', 'App_Purchase_003')
-GROUP BY A.age, A.gender
+GROUP BY A.age, A.gender 
 ORDER BY age_group, gender;
